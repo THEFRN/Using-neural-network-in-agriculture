3f414030a9e705859f0f04addc15300e8ce14b35
skywalker <farhangian.ahmad@gmail.com>
2023-01-19 15:07:23 +0330

    added required pics in to the directory

--- neural.py
+++ neural.py
@@ -1,26 +1,28 @@
 # Import Libraries
-import cv2
-
+import cv2 as cv
+# /////////////////////////////
 import matplotlib
 import matplotlib.pyplot as plt
-
+# /////////////////////////////
 import torch
 import torch.nn as nn
-
-import numpy as np
+# /////////////////////////////
+import sys
+sys.path.append('usr/local/lib/python3.10.2/site-packages')
 
 # Loading the image data
-image = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('ShowImage', image)
-cv2.waitkey(0)
-cv2.destroyAllwindows()
+image = cv.imread('1.jpg', cv.IMREAD_GRAYSCALE)
+cv.imshow('ShowImage', image)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
 batch_size = 64
 # Data Preparation
 n_input, n_hidden, n_out, batch_size, learning_rate = 10, 30, 1, 100, 0.1
 
-data_x = torch.randn(batch_size, n_input)
+data_x = image
 data_y = (torch.rand(size=(batch_size, 1)) < 0.5).float()
+
 
 print(data_x.size())
 print(data_y.size())
