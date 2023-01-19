b840ea2b635557732b10887d47737b5cdffcfeb0
skywalker <farhangian.ahmad@gmail.com>
2023-01-19 15:30:13 +0330

    loaded all pics in the directory

--- neural.py
+++ neural.py
@@ -1,33 +1,37 @@
 # Import Libraries
 import cv2 as cv
-# /////////////////////////////
+
 import matplotlib
 import matplotlib.pyplot as plt
-# /////////////////////////////
+
 import torch
 import torch.nn as nn
+
+import os
 # Adding images
-
+path = 'E:\\Neural Network Project\\First_project\\images'
+image_format = '.jpg'
+image_file_list = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(image_format)]
 # Loading the image data
-image = cv.imread('1.jpg', cv.IMREAD_GRAYSCALE)
-cv.imshow('ShowImage', image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+images = []
+for img in image_file_list:
+    print(img)
+    image = cv.imread(img)
+    images.append(image)
+    cv.imshow('Image', image)
+    cv.waitKey(0)
 
-batch_size = 64
+
 # Data Preparation
-n_input, n_hidden, n_out, batch_size, learning_rate = 10, 30, 1, 100, 0.1
+n_hidden, n_out, learning_rate, data_y = 30, 1, 0.1, 0
+for image in images:
+    data_x = image
+    data_y += 1
 
-data_x = image
-data_y = (torch.rand(size=(batch_size, 1)) < 0.5).float()
-
-
-print(data_x.size())
-print(data_y.size())
 
 # Defining Neural Network Model
 
-model = nn.Sequential(nn.Linear(n_input, n_hidden),
+model = nn.Sequential(nn.Linear(16, n_hidden),
                       nn.ReLU(),
                       nn.Linear(n_hidden, n_out),
                       nn.Sigmoid())
